4e84cfbf21e0bda996de97f7e204db37f55542e5
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-01 16:49:54 -0700

    added burger-menu.js and coded it up

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,3 +1,5 @@
+import getFocusableElements from './get-focusable-elements.js'; 
+
 class BurgerMenu extends HTMLElement {
     constructor() {
         super();
@@ -24,6 +26,120 @@ class BurgerMenu extends HTMLElement {
             }
         );
     }
+
+    processStateChange() {
+        this.root.setAttribute('status', this.state.status);
+
+        this.root.setAttribute('enabled', this.state.enabled ? 'true' : 'false');
+
+        this.manageFocus();
+
+        switch (this.state.status) {
+            case 'closed':
+                this.trigger.setAttribute('aria-label', 'Open menu');
+                this.trigger.setAttribute('aria-expanded', 'false');
+                break;
+            case 'iniital':
+            case 'open':
+                this.trigger.setAttribute('aria-label', 'Close menu');
+                this.trigger.setAttribute('aria-expanded', 'true');
+                break;
+        }
+    }
+
+    manageFocus() {
+        if (!this.state.enabled) {
+            this.focusableElements.forEach(element => element.removeAttribute('tabindex'));
+            return;
+        }
+
+        switch (this.state.status) {
+            case 'open':
+                this.focusableElements.forEach(element => element.removeAttribute('tabindex'));
+                break;
+            case 'closed':
+                [...this.focusableElements]
+                    .filter(element => element.getAttribute('data-element') !== 'burger-menu-trigger')
+                    .forEach(element => element.setAttribute('tabindex', '-1'));
+        }
+    }
+
+    get maxWidth() {
+        return parseInt(this.getAttribute('max-width') || 9999, 10);
+    }
+
+    connectedCallback() {
+        this.initialMarkup = this.innerHTML;
+
+        this.render();
+
+        const observer = new ResizeObserver(observedItems => {
+            const { contentRect } = observedItems[0];
+
+            this.state.enabled = contentRect.width <= this.maxWidth;
+        });
+
+        observer.observe(this.parentNode);
+    }
+
+    render() {
+        this.innerHTML = `
+            <div class="burger-menu" data-element="burger-root">
+                <button class="burger-menu__trigger" data-element="burger-menu-trigger" type="button" aria-label="Open menu">
+                    <span class="burger-menu__bars" aria-hidden="true"></span>
+                </button>
+                <div class="burger-menu__panel" data-element="burger-menu-panel">
+                    ${this.initialMarkup}
+                </div>
+            </div>
+        `;
+
+        this.postRender();     
+    }
+
+    postRender() {
+        this.root = document.querySelector('[data-element="burger-root"]');
+
+        this.trigger = document.querySelector('[data-element="burger-menu-trigger"');
+
+        this.panel = document.querySelector('[data-element="burger-menu-panel"]');
+
+        this.focusableElements = getFocusableElements(this);
+
+        if (this.trigger && this.panel) {
+            this.toggle();
+
+            this.trigger.addEventListener('click', evt => {
+                evt.preventDefault();
+
+                this.toggle();
+            });
+
+            document.addEventListener('focusin', () => {
+                if (!this.contains(document.activeElement)) {
+                    this.toggle('closed');
+                }
+            });
+
+            return;
+        }
+
+        this.innerHTML = this.initialMarkup;
+    }
+
+    toggle(forcedStatus) {
+        if (forcedStatus) {
+            if (this.state.status === forcedStatus) {
+                return;
+            }
+
+            this.state.status = 'closed';
+        } else {
+            this.state.status = this.state.status === 'closed' ? 'open' : 'closed';
+        }
+    }
 }
 
-customElements.define('burger-menu', BurgerMenu);
+if ('customElements' in window) {
+    customElements.define('burger-menu', BurgerMenu);
+}
