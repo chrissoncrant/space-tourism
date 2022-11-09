bb51afb42a00cc67392a9ce6bb1195ee17d13892
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-09 12:13:47 -0800

    added focus management code

--- js/script.js
+++ js/script.js
@@ -7,14 +7,56 @@ burgerButton.addEventListener('click', event => {
 
     const visibility = primaryNav.getAttribute('data-visible');
 
-    if (visibility === 'false') {
+    if (visibility === 'false') {                       
         primaryNav.setAttribute('data-visible', 'true');
 
         burgerButton.setAttribute('aria-expanded', 'true');
+
+        manageFocus();
     }
     else {
         primaryNav.setAttribute('data-visible', 'false');
 
         burgerButton.setAttribute('aria-expanded', 'false');
+        
+        manageFocus();
     }
 })
+
+const burgerBtnMql = window.matchMedia('(max-width: 35em)');
+
+function manageFocus() {
+     if (burgerBtnMql.matches) {
+        if (burgerButton.getAttribute('aria-expanded') === 'false') {
+            primaryNav.querySelectorAll('a').forEach(link => {
+                link.setAttribute('tabindex', '-1')
+            })
+        } else {
+            primaryNav.querySelectorAll('a').forEach(link => {
+                link.removeAttribute('tabindex');
+            })
+        };
+     } else {
+        primaryNav.querySelectorAll('a').forEach(link => {
+            link.removeAttribute('tabindex');
+        })
+     };
+}
+
+document.addEventListener('focusin', event => {
+    if (burgerBtnMql.matches && burgerButton.getAttribute('aria-expanded') === 'true') {
+        
+        if (!primaryNav.contains(document.activeElement) && event.target !== burgerButton) {
+            primaryNav.setAttribute('data-visible', 'false');
+
+            burgerButton.setAttribute('aria-expanded', 'false');
+        
+            manageFocus();
+        };
+    };
+});
+
+burgerBtnMql.addEventListener('change', manageFocus);
+
+manageFocus();
+
