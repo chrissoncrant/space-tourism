204837eef570682b482bb608aedffbc2b6dc4a05
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-10 17:03:26 -0800

    initial setup of destination code

--- js/script.js
+++ js/script.js
@@ -63,3 +63,59 @@ burgerBtnMql.addEventListener('change', manageFocus);
 
 manageFocus();
 
+// ####################################
+// DESTINATIONS BUTTONS FUNCTION
+// ####################################
+
+const destinationsArr = [
+    {
+        name: 'moon',
+        description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+        distance: '384,000 km',
+        travelTime: '3 days'
+    },
+    {
+        name: 'mars',
+        description: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+        distance: '225 mil. km',
+        travelTime: '9 months'
+    },
+    {
+        name: 'europa',
+        description: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+        distance: '628 mil. km',
+        travelTime: '3 years'
+    },
+    {
+        name: 'titan',
+        description: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+        distance: '1.6 bil. km',
+        travelTime: '7 years'
+    },
+]
+
+const destinationTabs = [...document.querySelector('.destination-tabs.tab-list').children];
+
+let initialDestination = destinationTabs[0];
+
+console.log(initialDestination)
+
+destinationTabs.forEach(dest => {
+    dest.addEventListener('click', () => {
+        if (dest === initialDestination) {
+            return;
+        } else {
+            initialDestination.setAttribute('aria-selected', 'false');
+
+            initialDestination = dest;
+
+            initialDestination.setAttribute('aria-selected', 'true');
+        }
+        
+        const destinationName = dest.textContent.toLowerCase();
+
+        const destinationObject = destinationsArr.filter(dest => dest.name === destinationName)[0];
+
+        console.log(destinationObject)
+    })
+})
