366510851bb63885b3888e391434c95b0d316cf6
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-11 13:25:00 -0800

    destination page javascript is now functional

--- js/script.js
+++ js/script.js
@@ -70,25 +70,29 @@ manageFocus();
 const destinationsArr = [
     {
         name: 'moon',
-        description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+        imageSrc: 'image-moon.webp',
+        summary: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
         distance: '384,000 km',
         travelTime: '3 days'
     },
     {
         name: 'mars',
-        description: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+        imageSrc: 'image-mars.webp',
+        summary: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
         distance: '225 mil. km',
         travelTime: '9 months'
     },
     {
         name: 'europa',
-        description: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+        imageSrc: 'image-europa.webp',
+        summary: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
         distance: '628 mil. km',
         travelTime: '3 years'
     },
     {
         name: 'titan',
-        description: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+        imageSrc: 'image-titan.webp',
+        summary: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
         distance: '1.6 bil. km',
         travelTime: '7 years'
     },
@@ -96,26 +100,48 @@ const destinationsArr = [
 
 const destinationTabs = [...document.querySelector('.destination-tabs.tab-list').children];
 
-let initialDestination = destinationTabs[0];
-
-console.log(initialDestination)
+//Setting Moon as the Initial Destination
+let selectedDestination = destinationTabs[0];
 
 destinationTabs.forEach(dest => {
     dest.addEventListener('click', () => {
-        if (dest === initialDestination) {
+        if (dest === selectedDestination) {
             return;
         } else {
-            initialDestination.setAttribute('aria-selected', 'false');
+            //Update Styles
+            selectedDestination.setAttribute('aria-selected', 'false');
 
-            initialDestination = dest;
+            selectedDestination = dest;
 
-            initialDestination.setAttribute('aria-selected', 'true');
+            selectedDestination.setAttribute('aria-selected', 'true');
+
+            //Get destination object
+            const destinationName = dest.textContent.toLowerCase();
+
+            const destinationObject = destinationsArr.filter(dest => dest.name === destinationName)[0];
+
+            //Update Image
+            const destinationImage = document.querySelector('.destination-image img');
+
+            destinationImage.setAttribute('src', `../images/destination/${destinationObject.imageSrc}`);
+
+            //Update Title and Summary
+            const destinationTitle = document.querySelector('.destination-info h2');
+
+            const destinationSummary = document.querySelector('.destination-info p');
+
+            destinationTitle.textContent = destinationObject.name;
+
+            destinationSummary.textContent = destinationObject.summary;
+            
+            //Update stats
+            const destinationDistance = document.querySelector('.destination-distance p');
+
+            const destinationTravelTime = document.querySelector('.destination-travel-time p');
+
+            destinationDistance.textContent = destinationObject.distance;
+
+            destinationTravelTime.textContent = destinationObject.travelTime;
         }
-        
-        const destinationName = dest.textContent.toLowerCase();
-
-        const destinationObject = destinationsArr.filter(dest => dest.name === destinationName)[0];
-
-        console.log(destinationObject)
     })
 })
