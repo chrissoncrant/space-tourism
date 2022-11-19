8f6c02948b3e80e579c2aa33cbabaebb75798e4a
Chris Soncrant <christopher.soncrant@gmail.com>
2022-11-19 12:23:32 -0800

    added progressive enhancement for image on destination page

--- js/destinations.js
+++ js/destinations.js
@@ -1,28 +1,32 @@
 const destinationsArr = [
     {
         name: 'moon',
-        imageSrc: 'image-moon.webp',
+        srcSet: 'image-moon.webp',
+        imageSrc: 'image-moon.png',
         summary: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
         distance: '384,000 km',
         travelTime: '3 days'
     },
     {
         name: 'mars',
-        imageSrc: 'image-mars.webp',
+        srcSet: 'image-mars.webp',
+        imageSrc: 'image-mars.png',
         summary: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
         distance: '225 mil. km',
         travelTime: '9 months'
     },
     {
         name: 'europa',
-        imageSrc: 'image-europa.webp',
+        srcSet: 'image-europa.webp',
+        imageSrc: 'image-europa.png',
         summary: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
         distance: '628 mil. km',
         travelTime: '3 years'
     },
     {
         name: 'titan',
-        imageSrc: 'image-titan.webp',
+        srcSet: 'image-titan.webp',
+        imageSrc: 'image-titan.png',
         summary: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
         distance: '1.6 bil. km',
         travelTime: '7 years'
@@ -52,7 +56,12 @@ destinationTabs.forEach(dest => {
             const destinationObject = destinationsArr.filter(dest => dest.name === destinationName)[0];
 
             //Update Image
+            const sourceSet = document.querySelector('.destination-image source');
+            console.log(sourceSet)
+
             const destinationImage = document.querySelector('.destination-image img');
+
+            sourceSet.setAttribute('srcSet', `../images/destination/${destinationObject.srcSet}`);
 
             destinationImage.setAttribute('src', `../images/destination/${destinationObject.imageSrc}`);
 
